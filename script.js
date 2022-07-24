72f53c5ff000e303b5c779cbaa9f53836e48f24b
Lekanjoy <90498638+Lekanjoy@users.noreply.github.com>
2022-07-24 01:33:12 +0100

    Fetched the data and made some changes to th links field

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const hamburgerMenu = document.getElementById('menu');
+const hamburgerMenu = document.getElementById("menu");
 const navMenu = document.getElementById("nav");
 
-hamburgerMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('show')
-
-})
+hamburgerMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
 
 // Scroll Header
 window.addEventListener("scroll", () => {
-    const header = document.getElementById('header')
+  const header = document.getElementById("header");
   if (window.scrollY > header.offsetHeight + 200) {
     header.classList.add("scroll");
   } else {
@@ -16,21 +15,85 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const copyBox = document.getElementById("copyText");
-const copyBtn = document.getElementById("copyBtn");
+const shortenURL = document.getElementById("shortenURL");
+const shortenBtn = document.getElementById("shortenBtn");
+const label = document.getElementById("label");
 
-copyBox.addEventListener('focusin', () => {
-    copyBox.select();
-});
+const clipboard = document.querySelector("#clipboard");
+const originalLinkField = document.getElementById("original");
+const shortLinkField = document.getElementById("short");
 
+let dataLinks = [];
 
-copyBtn.addEventListener('click',() => {
-    const copyText = copyBox.value;
-    copyBox.select();
-    navigator.clipboard.writeText(copyText);
-    copyBox.value = 'Copied!'
+shortenBtn.addEventListener("click", getShortLink);
+
+async function getShortLink() {
+  const userURL = shortenURL.value;
+  if (!userURL) {
+    label.style.display = "block";
+    shortenURL.style.border = "2px solid hsl(0, 87%, 67%)";
 
     setTimeout(() => {
-        copyBox.value = copyText;
+      label.style.display = " none";
+      shortenURL.style.border = "none";
     }, 2000);
-})
+
+    return;
+  }
+
+  try {
+    const response = await fetch(` https://api.shrtco.de/v2/shorten?url= ${userURL}`);
+    const data = await response.json();
+
+    //Pushing the object into an array so as to use the map method
+    dataLinks.push(data.result);
+    shortenBtn.textContent = "Shortening . . .";
+    //Displaying the Generated Links
+    displayShortLink(dataLinks);
+    shortenURL.value = '';
+    shortenBtn.textContent = "Shorten It!";
+
+
+
+  } catch (error) {
+    console.error(error);
+    label.textContent = 'Something went wrong! Please try again.'
+    shortenURL.style.border = "2px solid hsl(0, 87%, 67%)";
+    label.style.display = 'block';
+
+      setTimeout(() => {
+        label.style.display = " none";
+        shortenURL.style.border = "none";
+      }, 5000);
+
+    
+  }
+}
+
+
+
+function displayShortLink(links) {
+  const htmlString = links
+    .map((link) => {
+      return `
+            <div class="link flex w-full gap-y-1 relative pb-6">
+          <input id="original" type="text" value="${link.original_link}" class=" hidden outline-none rounded-lg rounded-r-none w-1/2 p-4 text-DarkViolet md:block">
+          <input id="short" type="text" value="${link.short_link}" class="w-full outline-none rounded-l-none rounded-lg p-4 text-Cyan md:w-1/2">
+          <button id="copyBtn"  class=" absolute px-4 py-2 bg-Cyan rounded-lg text-white  right-3 top-2">Copy</button>
+        </div>
+
+    
+    `;
+    })
+    .join("");
+
+  clipboard.innerHTML += htmlString;
+  
+    // function copyToClipboard() {
+    //   // shortLinkField.select();
+    //   document.execCommand("copy");
+    //   document.getElementById("copyBtn").textContent = "Copied";
+    //   shortenBtn.style.background = "hsl(260, 8%, 14%)";
+    // }
+  
+}
